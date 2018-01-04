cb8c66de21b948e95aafe3c6527213cc6c6ef4a5
Tai Kedzierski <tkedzierski@optos.com>
2018-01-04 14:29:06 +0000

    Installer should follow clean writing recommendations!

--- install.sh
+++ install.sh
@@ -1,45 +1,62 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
-libsurl="https://github.com/taikedz/bash-libs"
-libsdir=./bash-libs/libs
+set -euo pipefail
 
-if [[ ! -d "$libsdir" ]] && [[ ! -L "$libsdir" ]]; then
-	git clone "$libsurl" || {
-		echo "Could not clone default libraries repo [$libsurl]"
-		exit 1
-	}
-fi
+pull_libraries() {
+	if [[ ! -d "$libsdir" ]] && [[ ! -L "$libsdir" ]]; then
+		git clone "$libsurl" || {
+			echo "Could not clone default libraries repo [$libsurl]"
+			exit 1
+		}
+	fi
+}
 
-if [[ "$UID" == 0 ]]; then
-	export libs=/usr/local/lib/bbuild
-	binsd=/usr/local/bin
-	BASHRCPATH=/etc/bash.bashrc
-else
-	export libs="$HOME/.local/lib/bbuild"
-	binsd="$HOME/.local/bin"
-	BASHRCPATH="$HOME/.bashrc"
-fi
+set_paths() {
+	libsurl="https://github.com/taikedz/bash-libs"
+	libsdir=./bash-libs/libs
 
-if [[ ! "$PATH" =~ "$binsd" ]]; then
-	echo "export PATH=\$PATH:$binsd" >> "$BASHRCPATH"
-fi
+	if [[ "$UID" == 0 ]]; then
+		export libs=/usr/local/lib/bbuild
+		binsd=/usr/local/bin
+		BASHRCPATH=/etc/bash.bashrc
+	else
+		export libs="$HOME/.local/lib/bbuild"
+		binsd="$HOME/.local/bin"
+		BASHRCPATH="$HOME/.bashrc"
+	fi
+}
 
-if ! grep "BBPATH=" "$BASHRCPATH" -q; then
-	echo "export BBPATH=\"$libs\"" >> "$BASHRCPATH"
-fi
+environment_configuration() {
+	if [[ ! "$PATH" =~ "$binsd" ]]; then
+		echo "export PATH=\$PATH:$binsd" >> "$BASHRCPATH"
+	fi
 
-mkdir -p "$binsd"
+	if ! grep "BBPATH=" "$BASHRCPATH" -q; then
+		echo "export BBPATH=\"$libs\"" >> "$BASHRCPATH"
+	fi
 
-bash bash-libs/install.sh
+	mkdir -p "$binsd"
+}
 
-BUILDFILES=(src/bashdoc src/bbuild)
+run_build() {
+	bash bash-libs/install.sh
 
-BBPATH="$libsdir" bash bootstrap/bootstrap-bbuild5 "${BUILDFILES[@]}" "$@" || exit 1
-cp ./build-outd/bbuild ./build-outd/bashdoc "$binsd/"
+	BUILDFILES=(src/bashdoc src/bbuild)
 
-echo -e "\033[32;1mSuccessfully installed 'bbuild' to [$binsd]\033[0m"
+	BBPATH="$libsdir" bash bootstrap/bootstrap-bbuild5 "${BUILDFILES[@]}" "$@" || exit 1
+	cp ./build-outd/bbuild ./build-outd/bashdoc "$binsd/"
 
-if ! which shellcheck 2>&1 >/dev/null ; then
-	echo -e '\n\tConsider installing "shellcheck"\n'
-fi
+	echo -e "\033[32;1mSuccessfully installed 'bbuild' to [$binsd]\033[0m"
+
+	if ! which shellcheck 2>&1 >/dev/null ; then
+		echo -e '\n\tConsider installing "shellcheck"\n'
+	fi
+}
+
+main() {
+	cd "$(dirname "$0")"
+	set_paths
+	pull_libraries
+	environment_configuration
+	run_build
+}
