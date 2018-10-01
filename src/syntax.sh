bb8a3fe3cdcbb26c85ade21f6b887b53646ff29b
Tai Kedzierski <dch.tai@gmail.com>
2018-10-01 11:38:06 +0100

    additional implementation and test

--- src/syntax.sh
+++ src/syntax.sh
@@ -1,14 +1,33 @@
-### Syntax Subfeatures Usage:help
+### Syntax Post-processing Usage:help
 #
 # Bash Builder adds some post-processing to built files
 # as an option, to allow some extra features.
 #
 # You can opt to switch on all syntax post-processing
-# subfeatures ("syntax extensions")
+# subfeatures ("syntax extensions"), or individual ones
 #
-# Syntax extensions include:
-#
-# 'expandarg1':
+###/doc
+
+# TODO - there are several array-related shortcuts to add
+# * transparent handling of serialization
+#   * though this may be rendered less relevant with strict mode
+# * associative arrays
+
+bbuild:syntax_post() {
+	# Syntax post-processor
+	local target="${1:-}"; shift || :
+	
+	bbuild:syntax:expand_local "$target"
+	bbuild:syntax:expand_arg1 "$target"
+    bbuild:syntax:expand_function_signatures "$target"
+}
+
+bbuild:syntax:use() {
+    [[ "$BBUILD_syntax_features" =~ syntax ]] ||
+	[[ "$BBUILD_syntax_features" =~ "$1" ]]
+}
+
+### expandarg1 Usage:bbuild
 #
 # An option which allows safe assignment of the first argument, and
 # forces a shift. The shift itself may error, allowing it to be caught.
@@ -26,7 +45,14 @@
 # 			echo "Hello $person : $message"
 # 		}
 #
-# 'expandlocal':
+##/doc
+bbuild:syntax:expand_arg1() {
+	bbuild:syntax:use expandarg1 || return
+	# adjacent quotes prevent this code from mangling itself
+	sed -r 's/=\$''%1\s*/="${1:-}"; shift /g' -i "$1"
+}
+
+### expandlocal Usage:bbuild
 #
 # An option which allows quickly defining a local variable
 #
@@ -34,27 +60,62 @@
 #
 # 	You get   ===>   local myvar=
 #
+# 	Example:
+#
+# 	    myfunc() {
+# 	        # declare a local variable person
+# 	        $%person="$1"
+#
+# 	        echo "Hello $person"
+# 	    }
 ###/doc
-
-bbuild:syntax_post() {
-	# Syntax post-processor
-	local target="${1:-}"; shift || :
-	
-	bbuild:syntax:expand_local "$target"
-	bbuild:syntax:expand_arg1 "$target"
-}
-
-bbuild:syntax:use() {
-	[[ "$BBUILD_syntax_features" =~ "$1" ]]
-}
-
-bbuild:syntax:expand_arg1() {
-	bbuild:syntax:use syntax || bbuild:syntax:use expandarg1 || return
-	# adjacent quotes prevent this code from mangling itself
-	sed -r 's/=\$''%1\s*/="${1:-}"; shift /g' -i "$1"
-}
-
 bbuild:syntax:expand_local() {
-	bbuild:syntax:use syntax || bbuild:syntax:use expandlocal || return
-	sed -r 's/^(\s*)\$%([a-zA-Z0-9_]+)=/\1local \2=/g' -i "$1"
+	bbuild:syntax:use expandlocal || return
+	sed -r 's/^(\s*)\$''%([a-zA-Z0-9_]+)=/\1local \2=/g' -i "$1"
 }
+
+### expandfsig Usage:bbuild
+#
+# An option to use a function's signature to declare local variables
+#
+# NOTE: function names must match the following regex:
+#   ^[a-zA-Z0-9_:.-]+$
+#
+# 	You write ===>   $%function functionname(var1 var2 var3) {
+#
+# 	You get   ===>   functionname() { . <(args:use:local var1 var2 var3 -- "$@") ;
+#
+# 	Example:
+#
+# 	    $%myfunc(firstname lastname) {
+# 	        echo "Hello $lastname, $firstname - you have a new message!"
+# 	        echo "$*"
+# 	    }
+###/doc
+bbuild:syntax:expand_function_signatures() {
+	bbuild:syntax:use expandfsig || return
+    sed -r 's/^(\s*)\$''%function\s*([a-zA-Z0-9_:.-]+)\(([^)]+?)\)\s+\{''/''\1\2() {\n\1    . <(args:use:local \3 -- "$@") ; ''/' -i "$1"
+}
+
+###### +++++++++++++++++++++++++++
+
+### Example and test
+# Direct test
+#   (
+#       . src/syntax.sh
+#       bbuild test-syntax.sh
+#       BBUILD_syntax_features=syntax bbuild:syntax_post build-outd/test-syntax.sh # This will eventually integrate to bbuild itself
+#       build-outd/test-syntax.sh Alice Bob "extra data"
+#   )
+
+# Test data in "test-syntax.sh" :
+
+###%include args.sh
+##
+##$%function sayhello(name1 name2) {
+##	$%myvar=$%1
+##
+##	echo "Hello $name1 and $name2, myvar is $myvar"
+##}
+##
+##sayhello "$@"
