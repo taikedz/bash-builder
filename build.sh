950b8c73bd178beae55fb24cb88b9b5168343cf7
Tai Kedzierski <dch.tai@gmail.com>
2019-03-29 10:48:51 +0000

    Allow building individual targets

--- build.sh
+++ build.sh
@@ -7,16 +7,51 @@ if [[ "$*" =~ --release ]]; then
     export BUILDOUTD=bin/
 fi
 
-status=0
 
-bin/bbuild src/bbuild                  ; status=$((status+$?))
-bin/bbuild src/bbrun                   ; status=$((status+$?))
-bin/bbuild src/bashdoc                 ; status=$((status+$?))
-bin/bbuild src/tarshc                  ; status=$((status+$?))
-bin/bbuild src/install.sh ./install.sh ; status=$((status+$?))
+BUILDSTATUS=0
 
-[[ "$status" -le 0 ]] || {
-    echo "BUILD FAIL"
+_has() {
+    local needle="$1"; shift
+    local x
+
+    res=1
+    for x in "$@"; do
+        if [[ "$x" = "$needle" ]]; then
+            res=0
+        fi
+    done
+
+    return "$res"
+}
+
+_build() {
+    bin/bbuild "$@" || BUILDSTATUS=$((BUILDSTATUS+1))
+}
+
+SCRIPTS=(bbuild bbrun bashdoc tarshc)
+
+if [[ -n "$*" ]]; then # Build specific
+    for buildsrc in "${SCRIPTS[@]}"; do
+        if _has "$buildsrc" "$@"; then
+            _build "src/$buildsrc"
+        fi
+    done
+
+    if _has install "$@"; then
+        _build src/install.sh ./install.sh
+    fi
+
+else # Build all
+    for buildsrc in "${SCRIPTS[@]}"; do
+        _build "src/$buildsrc"
+    done
+
+    _build src/install.sh ./install.sh
+fi
+
+
+[[ "$BUILDSTATUS" -le 0 ]] || {
+    echo "BUILD FAIL -- $BUILDSTATUS failures"
     exit 1
 }
 
